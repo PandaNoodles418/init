37e5c253d42afedb3e67b031f6f6d5e0189d09aa
Panda Noodles <pandanoodles416@users.noreply.github.com>
2024-05-04 19:34:33 -0500

    || true

--- setup.sh
+++ setup.sh
@@ -8,44 +8,44 @@ echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # dev tools
-brew install git
-brew install --cask github
-brew install --cask visual-studio-code
-brew install --cask warp
+brew install git || true
+brew install --cask github || true
+brew install --cask visual-studio-code || true
+brew install --cask warp || true
 
 # browsers
-brew install --cask brave-browser
-brew install --cask firefox
-brew install --cask google-chrome
+brew install --cask brave-browser || true
+brew install --cask firefox || true
+brew install --cask google-chrome || true
 
 # JavaScript
-brew install node nvm jq oven-sh/bun/bun
-nvm install --lts
-nvm install 20
+brew install node nvm jq oven-sh/bun/bun || true
+nvm install --lts || true
+nvm install 20 || true
 
 # Docker
-brew install --cask docker
-brew install docker-completion docker-compose docker-credential-helper-ecr
+brew install --cask docker || true
+brew install docker-completion docker-compose docker-credential-helper-ecr || true
 
 # k8s
-brew install kubectl kind helm k9s
+brew install kubectl kind helm k9s || true
 
 # languages
-brew install python pyenv
-brew install go
+brew install python pyenv || true
+brew install go || true
 
 # infra
-brew install awscli aws-cdk aws-iam-authenticator aws-vault
-brew install terraform terragrunt
+brew install awscli aws-cdk aws-iam-authenticator aws-vault || true
+brew install terraform terragrunt || true
 
 # misc
-brew install --cask obsidian
-brew install --cask discord
-brew install --cask signal
-brew install --cask nordvpn
+brew install --cask obsidian || true
+brew install --cask discord || true
+brew install --cask signal || true
+brew install --cask nordvpn || true
 
 # ollama
-brew install ollama
+brew install ollama || true
 open -a "ollama"
 ollama pull codellama:latest
 ollama pull dolphin-mixtral:latest
